77a99f4bf784fc349ef148e239eb56d05b36dff0
Fadhili-code <f.lumumba@alustudent.com>
2025-07-06 13:28:10 +0300

    Fix Page Scroll Position on Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AdminProvider } from './context/AdminContext';
+import ScrollToTop from './components/ScrollToTop';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Register from './pages/Register';
@@ -12,6 +13,7 @@ function App() {
   return (
     <AdminProvider>
       <Router>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
